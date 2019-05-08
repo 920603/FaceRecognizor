1f2595d111fa4dc7cedca35910c7da3cb05b4d03
Sanghyun PARK <sanghyun.park.dev@gmail.com>
2019-05-08 14:59:16 +0200

    first commit

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Tilt from "react-tilt";
+import FF_Logo from "./FF_Logo.png";
+
+const Logo = () => {
+  return (
+    <div className="ma4 mt0">
+      <Tilt
+        className="Tilt br2 shadow-2"
+        options={{ max: 55 }}
+        style={{ height: 150, width: 150 }}
+      >
+        <div className="Tilt-inner pa4">
+          <img src={FF_Logo} alt="FaceFinder Logo" />
+        </div>
+      </Tilt>
+    </div>
+  );
+};
+
+export default Logo;
